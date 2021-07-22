2baacfc74152ec8dde949e3d31f611ab8d0ea067
dzungnt2412 <dzungnt2412@gmail.com>
2021-07-22 14:58:42 +0700

    audit log

--- src/packages/package/constants.js
+++ src/packages/package/constants.js
@@ -154,6 +154,13 @@ export const CHANGE_PACKAGE_TYPE = [
   'Dịch vụ',
   'Chi tiết hàng hóa',
   'Địa chỉ phụ người nhận',
+  'Sửa phí phát sinh do Covid',
+  'Sửa phí phát sinh quá cỡ',
+  'Sửa phí phát sinh sửa trọng lượng',
+  'Sửa phí phát sinh sửa kích thước',
+  'Sửa phí phát sinh dịch vụ',
+  'Sửa phí phát sinh sửa đơn',
+  'Sửa phí phát sinh hoàn tiền',
 ]
 
 export const DELIVER_LOG_PACKAGE = {
