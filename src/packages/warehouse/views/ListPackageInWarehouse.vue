bf9998461be3b697eaff28aab55fb2ef9ab43244
khoand <neu.khoanguyen@gmail.com>
2021-07-27 16:31:10 +0700

    update print

--- src/packages/warehouse/views/ListPackageInWarehouse.vue
+++ src/packages/warehouse/views/ListPackageInWarehouse.vue
@@ -38,7 +38,6 @@
                       <th width="100">Dịch vụ</th>
                       <th>Nhãn đơn</th>
                       <th>Nhãn kiện</th>
-                      <th>ID kiện</th>
                       <th>Mã kiện</th>
                       <th>ID lô</th>
                       <th></th>
@@ -99,29 +98,14 @@
                     >
                     <td>
                       <router-link
-                        v-if="item.container_id"
                         class="text-no-underline"
                         :to="{
                           name: 'container-detail',
                           params: { id: item.container_id },
                         }"
                       >
-                        {{ item.container_id }}
+                        C{{ item.container_id }}
                       </router-link>
-                      <span v-else>-</span>
-                    </td>
-                    <td>
-                      <router-link
-                        v-if="item.container_code"
-                        class="text-no-underline"
-                        :to="{
-                          name: 'container-detail',
-                          params: { id: item.container_id },
-                        }"
-                      >
-                        {{ item.container_code }}
-                      </router-link>
-                      <span v-else>-</span>
                     </td>
                     <td>
                       <router-link
