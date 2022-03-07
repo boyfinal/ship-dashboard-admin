7627fba4c103093bc787a7ad556ff3fa41dc37fe
Do Hai Hung 20161927 <hung.dh161927@sis.hust.edu.vn>
2022-03-07 13:48:25 +0700

    fix side bar

--- src/packages/bill/routes.js
+++ src/packages/bill/routes.js
@@ -1,19 +1,19 @@
-export const routes = [
-  {
-    path: '/bills',
-    name: 'bill-list',
-    meta: {
-      title: 'Danh sách hóa đơn',
+export const routes = [{
+        path: '/bills',
+        name: 'bill-list',
+        meta: {
+            title: 'Quản lí tài chính',
+        },
+        component: () =>
+            import ( /* webpackChunkName: "bills" */ './views/List.vue'),
     },
-    component: () => import(/* webpackChunkName: "bills" */ './views/List.vue'),
-  },
-  {
-    path: '/bills/:code',
-    name: 'bill-detail',
-    meta: {
-      title: 'Chi tiết hóa đơn',
+    {
+        path: '/bills/:code',
+        name: 'bill-detail',
+        meta: {
+            title: 'Chi tiết hóa đơn',
+        },
+        component: () =>
+            import ( /* webpackChunkName: "bills" */ './views/BillDetail.vue'),
     },
-    component: () =>
-      import(/* webpackChunkName: "bills" */ './views/BillDetail.vue'),
-  },
 ]
