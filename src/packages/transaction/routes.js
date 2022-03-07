d5192dc43968236461027ab83555fab1f508625e
Do Hai Hung 20161927 <hung.dh161927@sis.hust.edu.vn>
2022-03-07 13:37:27 +0700

    fix side bar

--- src/packages/transaction/routes.js
+++ src/packages/transaction/routes.js
@@ -1,13 +1,12 @@
-export const routes = [
-  {
+export const routes = [{
     path: '/transactions',
     name: 'list-transaction',
     meta: {
-      title: 'Thanh toán',
+        title: 'Topup',
     },
     component: () =>
-      import(
-        /* webpackChunkName: "transaction" */ './views/ListTransactions.vue'
-      ),
-  },
-]
+        import (
+            /* webpackChunkName: "transaction" */
+            './views/ListTransactions.vue'
+        ),
+}, ]
