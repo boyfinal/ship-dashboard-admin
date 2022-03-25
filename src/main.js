68d298b9981088834246b8a1c1dde6693a8dcb76
Khoa Nguyen <neu.khoanguyen@gmail.com>
2022-03-25 16:56:48 +0700

    update view

--- src/main.js
+++ src/main.js
@@ -11,8 +11,6 @@ import store from './core/store'
 import '../uikit'
 import './core/icons'
 import { VclTable } from 'vue-content-loading'
-import InlineSvg from 'vue-inline-svg'
-Vue.component('inline-svg', InlineSvg)
 Vue.use(VueClipboard)
 
 Vue.use(Notifications)
