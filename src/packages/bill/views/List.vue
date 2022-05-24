d528a23e8082a9ee9c4550032ff6e0b68ffce6d8
Khoa Nguyen <neu.khoanguyen@gmail.com>
2022-05-24 15:21:51 +0700

    update view

--- src/packages/bill/views/List.vue
+++ src/packages/bill/views/List.vue
@@ -715,7 +715,8 @@ export default {
       return (
         (transaction.type === TransactionLogTypePayoneer ||
           transaction.type === TransactionLogTypePingPong) &&
-        transaction.status === TransactionStatusProcess
+        transaction.status === TransactionStatusProcess &&
+        transaction.amount <= 0
       )
     },
     getTextType(transaction) {
