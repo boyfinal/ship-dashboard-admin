18e4cf21b314c2a8518b1ae97a62b7e2b6c28312
Do Hai Hung 20161927 <hung.dh161927@sis.hust.edu.vn>
2022-07-07 15:47:43 +0700

    close container

--- src/packages/container/components/ModalChoiceShippingBox.vue
+++ src/packages/container/components/ModalChoiceShippingBox.vue
@@ -124,6 +124,15 @@
           />
         </div>
       </div>
+      <template v-if="typeContainer == 1">
+        <br />
+        <div class="row">
+          <div class="col-12">
+            <label for=""><b>Mã tracking</b></label>
+            <p-input v-model="tracking_number" :error="err" />
+          </div>
+        </div>
+      </template>
     </div>
 
     <template slot="footer">
@@ -179,6 +188,9 @@ export default {
     title: {
       type: String,
     },
+    typeContainer: {
+      type: Number,
+    },
   },
   created() {
     this.valider = valider.schema((y) => ({
@@ -227,15 +239,19 @@ export default {
         max_weight: 0,
       },
       weight: 0,
+      tracking_number: '',
       warehouse: {},
       type: 0,
       warehouseID: 0,
       store: 1,
       valider: null,
+      err: '',
     }
   },
   methods: {
     handleClose() {
+      this.err = ''
+      this.valider.reset()
       this.$emit('update:visible', false)
     },
     async handleSave() {
@@ -249,20 +265,27 @@ export default {
           return
         }
       }
+      this.box = Object.assign(this.box, {
+        weight: this.weight,
+        tracking_number: this.tracking_number,
+      })
       if (!this.valider.check(this.box) && !this.isCreate) {
+        return
+      }
+      if (this.typeContainer == 1 && this.tracking_number == '') {
+        this.err = 'Tracking number không để trống'
         return
       }
       let payload = {}
       if (this.isCreate) {
-        payload = { ...{ warehouse_id: this.warehouseID } }
+        payload = { warehouse_id: this.warehouseID }
       } else {
         payload = {
-          ...{
-            width: this.box.width,
-            height: this.box.height,
-            length: this.box.length,
-            weight: this.weight,
-          },
+          width: this.box.width,
+          height: this.box.height,
+          length: this.box.length,
+          weight: this.box.weight,
+          tracking_number: this.box.tracking_number,
         }
       }
 
