1491900d5c279bcf7592af535fb1a563faef7c29
Khoa Nguyen <neu.khoanguyen@gmail.com>
2023-02-03 10:20:36 +0700

    update sidebar

--- src/packages/hub/routes.js
+++ src/packages/hub/routes.js
@@ -1,30 +1,30 @@
 export const routes = [
-  {
-    path: '/hubs/search',
-    name: 'hubs-search',
-    meta: {
-      title: 'Tra cứu đơn',
-    },
-    component: () => import(/* webpackChunkName: "hub" */ './views/Search.vue'),
-  },
-  {
-    path: '/hub/import',
-    name: 'scan-import-hub',
-    meta: {
-      title: 'Quét nhập hub',
-    },
-    component: () =>
-      import(/* webpackChunkName: "sign-in" */ './views/ScanImportHub'),
-  },
-  {
-    path: '/hub/export',
-    name: 'scan-export-hub',
-    meta: {
-      title: 'Quét xuất hub',
-    },
-    component: () =>
-      import(/* webpackChunkName: "sign-in" */ './views/ScanExportHub'),
-  },
+  // {
+  //   path: '/hubs/search',
+  //   name: 'hubs-search',
+  //   meta: {
+  //     title: 'Tra cứu đơn',
+  //   },
+  //   component: () => import(/* webpackChunkName: "hub" */ './views/Search.vue'),
+  // },
+  // {
+  //   path: '/hub/import',
+  //   name: 'scan-import-hub',
+  //   meta: {
+  //     title: 'Quét nhập hub',
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "sign-in" */ './views/ScanImportHub'),
+  // },
+  // {
+  //   path: '/hub/export',
+  //   name: 'scan-export-hub',
+  //   meta: {
+  //     title: 'Quét xuất hub',
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "sign-in" */ './views/ScanExportHub'),
+  // },
   {
     path: '/hub/return',
     name: 'hub-return',
@@ -34,13 +34,13 @@ export const routes = [
     component: () =>
       import(/* webpackChunkName: "sign-in" */ './views/PackageReturn'),
   },
-  {
-    path: '/hub/reship',
-    name: 'hub-reship',
-    meta: {
-      title: 'Hàng reship',
-    },
-    component: () =>
-      import(/* webpackChunkName: "sign-in" */ './views/PackageReship'),
-  },
+  // {
+  //   path: '/hub/reship',
+  //   name: 'hub-reship',
+  //   meta: {
+  //     title: 'Hàng reship',
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "sign-in" */ './views/PackageReship'),
+  // },
 ]
