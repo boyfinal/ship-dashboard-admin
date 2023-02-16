96afad235fe470d0ccaebfaa1f478079139930c3
Khoa Nguyen <neu.khoanguyen@gmail.com>
2023-02-16 11:23:42 +0700

    update

--- src/packages/package/views/ListAlertPackages.vue
+++ src/packages/package/views/ListAlertPackages.vue
@@ -455,6 +455,9 @@ export default {
   watch: {
     filter: {
       handler: function () {
+        if (this.isFetching) {
+          return
+        }
         this.init()
       },
       deep: true,
