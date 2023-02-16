ca926ae5defacfcef995f069508f34bf5d186091
Khoa Nguyen <neu.khoanguyen@gmail.com>
2023-02-16 11:31:56 +0700

    update dev-eks

--- src/packages/setting/views/Account.vue
+++ src/packages/setting/views/Account.vue
@@ -365,6 +365,9 @@ export default {
   watch: {
     filter: {
       handler: function () {
+        if (this.isFetching) {
+          return
+        }
         this.init()
       },
       deep: true,
