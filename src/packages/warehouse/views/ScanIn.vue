72b3b89a6bed5272eb1a2cb7a00988ee9ff6a872
VanHanh <vanhanh137@gmail.com>
2023-03-09 11:39:53 +0700

    fix scan barcode

--- src/packages/warehouse/views/ScanIn.vue
+++ src/packages/warehouse/views/ScanIn.vue
@@ -723,6 +723,7 @@ export default {
       } else if (keyword.length > 24) {
         keyword = keyword.slice(-22).trim()
       }
+
       this.keyword = keyword
       this.beforeFetchPackage(this.keyword)
       this.disableInput = false
@@ -774,9 +775,11 @@ export default {
       this.iscaned = false
       this.reset()
       keyword = keyword.trim()
-      if (keyword.length > 40) {
+      if (keyword.length == 38) {
+        keyword = keyword.slice(-26).trim()
+      } else if (keyword.length > 40) {
         keyword = keyword.slice(-23).trim()
-      } else if (keyword.length > 24) {
+      } else if (keyword.length > 24 && keyword.length != 26) {
         keyword = keyword.slice(-22).trim()
       }
       const res = await this.fetchPackage(keyword)
